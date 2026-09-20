c744a5b7664361a4dab54ca1837c511b13931380
zeykeenmd <zeykeenmdz@gmail.com>
2026-09-20 15:43:32 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -38,7 +38,7 @@ UPDATE_CODE_URL = os.getenv("UPDATE_CODE_URL", "").strip()
 UPDATE_INTERVAL = int(os.getenv("UPDATE_INTERVAL", "300"))  # secondes
 
 START_TIME = time.time()
-BOT_VERSION = "1.1.0"
+BOT_VERSION = "2.0.0"
 BOT_CREATOR = "9kr"
 try:
     BOT_FILE = os.path.abspath(__file__)
